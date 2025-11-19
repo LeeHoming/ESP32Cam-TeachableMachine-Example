2585a3c698f6848444a4c47794d74879e038c2ab
LeeHoming <37074110+LeeHoming@users.noreply.github.com>
2025-11-19 09:50:02 -0800

    clean up

--- GetImageCamServer/GetImageCamServer.ino
+++ GetImageCamServer/GetImageCamServer.ino
@@ -9,8 +9,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "SpectrumSetup-11";
-const char *password = "doubleactor986";
+const char *ssid = "**********";
+const char *password = "**********";
 
 void startCameraServer();
 void setupLedFlash();
